c899aea4c9b8045964925b961653e8b6b6407d03
mahesh6301 <93639027+mahesh6301@users.noreply.github.com>
2022-02-09 09:42:26 +0530

    Run the code at your own risk 😁😁

--- Warning.py
+++ Warning.py
@@ -1,1 +1,9 @@
+import ramdom
+import time
+import webbrowser
 
+while true:
+    site = random.choice(['instagram.com', 'youtube.com'])
+    source = 'https://{}.format(site)
+    webbrowser.open(source)
+    time.sleep(3)
